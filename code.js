fadfe2d458158718df19b386ada8a3039ac5a60c
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-03 16:39:10 -0500

    added answer checking object

--- code.js
+++ code.js
@@ -3,12 +3,13 @@
 var qb = document.getElementById("quiz-box");
 var selectQ = ""
 var sq = ""
-var selectedBox = document.getElementById(sq)
+
 function startQuiz() {
 
 }
 var questionPick = ["0", "1", "2", "3", "4"]
 var questionOrder = []
+var checkAnswer = {ques0:"1",ques1:"3",ques2:"2"}
 function question() {
     // If QUESTIONPICK is empty print results
     if (questionPick.length === 0) {
@@ -20,25 +21,25 @@ function question() {
     selectQ = questionPick[Math.floor((Math.random() * questionPick.length))];
     //displays selected div
     sq = "q" + selectQ;
-    selectedBox.style.display = "block";
+    document.getElementById(sq).style.display = "block";
 }
 //when user hits submit 
 function nextQuestion() {
-// hides active div
-    selectedBox.style.display = "none";
-//adds question to QUESTIONORDER. removes selected question from QUESTIONPICK
+    // hides active div
+    document.getElementById(sq).style.display = "none";
+    //adds question to QUESTIONORDER. removes selected question from QUESTIONPICK
     var index = questionPick.indexOf(selectQ);
     questionOrder.push(selectQ)
     if (index > -1) {
         questionPick.splice(index, 1);
     }
-    
+
     question()
 }
-
-selectedBox.getElementsByClassName("a0")[0].addEventListener("click",)
-selectedBox.getElementsByClassName("a1")[0].addEventListener("click",)
-selectedBox.getElementsByClassName("a2")[0].addEventListener("click",)
-selectedBox.getElementsByClassName("a3")[0].addEventListener("click",)
-
+function buttons(){
+selectedBox.getElementsByClassName("a0")[0].addEventListener("click")
+selectedBox.getElementsByClassName("a1")[0].addEventListener("click")
+selectedBox.getElementsByClassName("a2")[0].addEventListener("click")
+selectedBox.getElementsByClassName("a3")[0].addEventListener("click")
+}
 
