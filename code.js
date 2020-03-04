806882d20872da61f5a0bf8db6e716453e90d6cc
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-04 17:44:46 -0500

    created function to input user answer to object based on the question number

--- code.js
+++ code.js
@@ -10,6 +10,12 @@ function startQuiz() {
 var questionPick = ["0", "1", "2", "3", "4"]
 var questionOrder = []
 var checkAnswer = {ques0:"1",ques1:"3",ques2:"2"}
+var uInput ={
+    u0:"",
+    u1:"",
+    u2:"",
+    u3:"",
+}
 function question() {
     // If QUESTIONPICK is empty print results
     if (questionPick.length === 0) {
@@ -21,10 +27,15 @@ function question() {
     selectQ = questionPick[Math.floor((Math.random() * questionPick.length))];
     //displays selected div
     sq = "q" + selectQ;
+    ui = "u" + selectQ
     document.getElementById(sq).style.display = "block";
 }
 //when user hits submit 
 function nextQuestion() {
+    if(uInput[ui] = ""){
+        console.log("select an answer");
+        return 0;
+    }
     // hides active div
     document.getElementById(sq).style.display = "none";
     //adds question to QUESTIONORDER. removes selected question from QUESTIONPICK
@@ -36,10 +47,18 @@ function nextQuestion() {
 
     question()
 }
-function buttons(){
-document.getElementById(sq).getElementsByClassName("a0")[0].addEventListener("click")
-document.getElementById(sq).getElementsByClassName("a1")[0].addEventListener("click")
-document.getElementById(sq).getElementsByClassName("a2")[0].addEventListener("click")
-document.getElementById(sq).getElementsByClassName("a3")[0].addEventListener("click")
+// takes INPUT from button. sets UINPUT[SELECTED QUESTION] to button input
+function ans(input){
+uInput[ui] = input
 }
+
+    
+
+// document.getElementById("q0").getElementsByClassName("a0")[0].addEventListener("click",function(){
+//     console.log("button one works")
+// })
+// document.getElementById(sq).getElementsByClassName("a1")[0].addEventListener("click")
+// document.getElementById(sq).getElementsByClassName("a2")[0].addEventListener("click")
+// document.getElementById(sq).getElementsByClassName("a3")[0].addEventListener("click")
+
 
