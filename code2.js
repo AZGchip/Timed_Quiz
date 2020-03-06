ab0acbe61ef34dbcf7d68e2620ae75878046c775
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-06 11:38:32 -0500

    added function that writes each question and adds click events to answers

--- code2.js
+++ code2.js
@@ -1,27 +1,56 @@
 //questions and answers
-var quizQuestions = ["1+1=","2+2=","3+3=","4+4=","5+5="]
-var answer0 = ["2","11","45","10"]
-var answer1 = ["4","22","8","0"]
-var answer2 = ["6","9","2","5"]
-var answer3 = ["8","9","10","11"]
-var answer4 = ["10","5","55","15"]
+var quizQuestions = ["1+1=", "2+2=", "3+3=", "4+4=", "5+5="];
+var answer0 = ["2", "11", "45", "10"];
+var answer1 = ["4", "22", "8", "0"];
+var answer2 = ["6", "9", "2", "5"];
+var answer3 = ["8", "9", "10", "11"];
+var answer4 = ["10", "5", "55", "15"];
+var answerObj = [answer0, answer1, answer2, answer3, answer4];
+var userAnswer = [];
 
-var questAmount = quizQuestions.length
-var q
+var questAmount = quizQuestions.length;
+var q = 0;
 
-function quizStart(){
-timeStart(100)
-for (q = 0; q < questAmount;q++){
+// function quizStart() {
+//     timeStart(100)
+nextQuestion()
+function nextQuestion() {
+
+    var selAns = answerObj[q]
+    $("#quizbox").append("<h1 class='question'>" + quizQuestions[q] + "</h1>")
+    for (var a = 0; a < selAns.length; a++) {
+        var quizButton = $("<div class='button'>");
+        $(quizButton).attr("answer", selAns[a]);
+        $(quizButton).text(selAns[a]);
+        $("#quizbox").append(quizButton)
+    }
+    $(".button").on("click", function () {
+        userAnswer.push($(this).attr("answer"));
+        q++;
+        $("#quizbox").empty();
+        if(q < quizQuestions.length){
+        nextQuestion()   
+        }
+        if(q > quizQuestions.length){
+        quizEnd()
+        }
+        
+    })
+
 
 }
+function quizEnd(){
+    for (var i = 0; i< quizQuestions.length; i++) {
+        
+        
+    }
 }
 
+function timeStart(time) {
+    setInterval(function () {
+        time--
 
-function timeStart(time){
-setInterval(function(){
-    time--
-
-}, 1000);
+    }, 1000);
 }
 
 
